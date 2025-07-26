0ea5d3bb970fd074daf491315359481fc8a9e761
Sahith-2525 <2420090084@klh.edu.in>
2025-07-26 11:14:21 +0530

    Add files via upload

---
+++ ternaryoperators.java
@@ -0,0 +1,16 @@
+package oppspractical;
+import java.util.Scanner;
+public class ternaryoperators {
+
+		  public static void main(String[] args) {
+
+		    int februaryDays = 29;
+		    String result;
+
+		    
+		    result = (februaryDays == 28) ? "Not a leap year" : "Leap year";
+		    System.out.println(result);
+		  }
+		}
+
+
