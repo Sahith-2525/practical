deeabe57f76789ab35b9e8abded4ac39e579eca0
Sahith-2525 <2420090084@klh.edu.in>
2025-07-26 11:11:58 +0530

    Add files via upload

---
+++ bitwiseoperators.java
@@ -0,0 +1,18 @@
+package oppspractical;
+import java.util.Scanner;
+public class bitwiseoperators {
+
+		   public static void main(String args[]) {
+		      int a = 60;	
+		      int b = 13;	
+		      int c = 0;
+
+		      c = a ^ b;        
+		      System.out.println("a ^ b = " + c );
+
+		      c = ~a;           
+		      System.out.println("~a = " + c );
+		   }
+		}
+
+
