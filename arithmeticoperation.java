1195a55ee9ab34f4050cd20a052c07a8604d375c
Sahith-2525 <2420090084@klh.edu.in>
2025-07-26 10:48:16 +0530

    Add files via upload

---
+++ arithmeticoperation.java
@@ -0,0 +1,26 @@
+package oppspractical;
+import java.util.Scanner;
+
+public class arithmeticoperation {
+
+	  public static void main(String[] args) {
+	    
+	    
+	    int a = 12, b = 5;
+
+	    
+	    System.out.println("a + b = " + (a + b));
+
+	    
+	    System.out.println("a - b = " + (a - b));
+
+	    
+	    System.out.println("a * b = " + (a * b));
+
+	    
+	    System.out.println("a / b = " + (a / b));
+
+	    
+	    System.out.println("a % b = " + (a % b));
+	  }
+	}
